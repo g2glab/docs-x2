facc6a932ca30d6c15528e77323e96f773181fc0
ryotayamanaka <ryotayamanaka@gmail.com>
2019-10-12 16:02:01 +0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -85,7 +85,11 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+  'logo_only': True,
+  'display_version': False
+}
+html_logo = 'img/g2glab_x2.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -133,7 +137,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'g2gml.tex', 'X2 Documentation',
+    (master_doc, 'x2.tex', 'X2 Documentation',
      'g2glab', 'manual'),
 ]
 
